8d3a7449dbfa80a1e743179c6617faa027a4d126
Julio Cesar <billyjoplin112@gmail.com>
2025-05-05 13:45:25 -0600

    added databaseUrl for connection with database

--- src/config/database.js
+++ src/config/database.js
@@ -1,18 +1,25 @@
 const { Sequelize } = require("sequelize");
-const config = require("./env")
+const config = require("./env");
 
-const db = new Sequelize(config.db.dbName, config.db.username, config.db.password, {
-  host: config.db.host,
-  dialect: "postgres",
-  dialectOptions:
-    process.env.NODE_ENV === "production"
-      ? {
-        ssl: {
-          require: true,
-          rejectUnauthorized: false
-        }
-      } : {}
-});
+let db;
 
+if (config.db_url) {
+  // Si existe DATABASE_URL, úsala directamente (modo producción)
+  db = new Sequelize(config.db_url, {
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+} else {
+  // Si no hay DATABASE_URL, usa los valores locales
+  db = new Sequelize(config.db.dbName, config.db.username, config.db.password, {
+    host: config.db.host,
+    dialect: "postgres",
+  });
+}
 
-module.exports = db
+module.exports = db;
