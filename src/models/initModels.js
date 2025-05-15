091900bf506265fca5987d8f1b62f3f348378ae4
Julio Cesar <billyjoplin112@gmail.com>
2025-05-15 12:33:08 -0600

    feat(cart) hice mis asociasiones en initModels

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -1,3 +1,4 @@
+const Cart = require("./cartModel");
 const Category = require("./categoryModel");
 const Products = require("./productModel");
 const Users = require("./userModel");
@@ -5,6 +6,9 @@ const Users = require("./userModel");
 const initModels = () => {
   Products.belongsTo(Category);
   Category.hasMany(Products);
+
+  Cart.belongsTo(Users);
+  Users.hasMany(Cart);
 };
 
 module.exports = initModels;
