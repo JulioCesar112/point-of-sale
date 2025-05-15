a8b93d97950e398d1f8dd5a409918b3bb104d243
Julio Cesar <billyjoplin112@gmail.com>
2025-05-15 12:06:59 -0600

    feat(products): add aminvalidate

--- src/routes/productRouter.js
+++ src/routes/productRouter.js
@@ -2,15 +2,39 @@ const express = require("express");
 const router = express.Router();
 const productService = require("../services/productService");
 
+const passport = require("passport");
+require("../middlewares/authMiddleware")(passport);
+const { adminValidate } = require("../middlewares/isAdmin");
+
 router
   .route("/")
-  .get(productService.getAllProducts)
-  .post(productService.postProduct);
+  .get(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    productService.getAllProducts
+  )
+  .post(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    productService.postProduct
+  );
 
 router
   .route("/:id")
-  .get(productService.getProductById)
-  .delete(productService.deleteProduct)
-  .patch(productService.updateProduct);
+  .get(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    productService.getProductById
+  )
+  .delete(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    productService.deleteProduct
+  )
+  .patch(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    productService.updateProduct
+  );
 
 module.exports = router;
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -3,21 +3,28 @@ const router = express.Router(); // Create a new router instance
 const userService = require("../services/userService"); // Import user service functions
 //! Rutas protegidas
 const passport = require("passport");
-require("../middlewares/authMiddleware")(passport)
+require("../middlewares/authMiddleware")(passport);
 const { adminValidate } = require("../middlewares/isAdmin");
 
-
 //! Root Routes
-router.get("/", userService.getAllUsers)
+router.get("/", userService.getAllUsers);
 
-//? Ruta de informacion propia del usuario logeado 
-router.route("/me")
+//? Ruta de informacion propia del usuario logeado
+router
+  .route("/me")
   .get(passport.authenticate("jwt", { session: false }), userService.getMyUser)
-  .patch(passport.authenticate("jwt", { session: false }), userService.updateMyUser)
-  .delete(passport.authenticate("jwt", { session: false }), userService.deleteMyUser)
+  .patch(
+    passport.authenticate("jwt", { session: false }),
+    userService.updateMyUser
+  )
+  .delete(
+    passport.authenticate("jwt", { session: false }),
+    userService.deleteMyUser
+  );
 
 //! Dynamic Routes
-router.route("/:id") // Define routes that require an ID parameter
+router
+  .route("/:id") // Define routes that require an ID parameter
   .get(userService.getUserById)
   .patch(
     passport.authenticate("jwt", { session: false }),
@@ -28,13 +35,14 @@ router.route("/:id") // Define routes that require an ID parameter
     passport.authenticate("jwt", { session: false }),
     adminValidate,
     userService.deleteUser
-  )
+  );
 
-router.route("/:id/updateUserRole")
-.put(
-  passport.authenticate("jwt",{session: false}),
-  adminValidate,
-  userService.updateUserRole
-)
+router
+  .route("/:id/updateUserRole")
+  .put(
+    passport.authenticate("jwt", { session: false }),
+    adminValidate,
+    userService.updateUserRole
+  );
 
 module.exports = router; // Export the router to use in other parts of the application
